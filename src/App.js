f35acf45ae50a224ece080e07e41853f6cdd28be
emmanuelmaachi <emmanuelmaachi@gmail.com>
2022-10-07 23:13:25 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import Header from './components/Header'
+import Tabs from './components/Tabs'
+import { indexerClient, myAlgoConnect } from "./utils/constants";
+import { buyCarAction, createcarAction, dislikeCarAction, getCarsAction, likeCarAction } from "./utils/spotter";
+
 
 function App() {
+  const [address, setAddress] = useState(null);
+  const [cars, setCars] = useState([]);
+  const [balance, setBalance] = useState(0);
+
+  const fetchBalance = async (accountAddress) => {
+    indexerClient.lookupAccountByID(accountAddress).do()
+      .then(response => {
+        const _balance = response.account.amount;
+        setBalance(_balance);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const connectWallet = async () => {
+    myAlgoConnect.connect()
+      .then(accounts => {
+        const _account = accounts[0];
+        console.log(_account)
+        setAddress(_account.address);
+        fetchBalance(_account.address);
+        if (_account.address) getCars();
+      }).catch(error => {
+        console.log('Could not connect to MyAlgo wallet');
+        console.error(error);
+      })
+  };
+
+  const addCar = async (data) => {
+    try {
+      await createcarAction(address, data)    
+    } catch (error) {
+      console.log(error);
+    } finally {
+      getCars();
+      fetchBalance()
+    }
+  };
+
+  const getCars = async () => {
+    try {
+      const cars = await getCarsAction();
+      if(cars)setCars(cars)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const likeCar = async (car) => {
+    try {
+      await likeCarAction(address, car)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      getCars();
+    }
+  };
+  const dislikeCar = async (car) => {
+    try {
+      await dislikeCarAction(address, car)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      getCars();
+    }
+  };
+
+  const buyCar = async (car) => {
+    try {
+      await buyCarAction(address, car)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      getCars();
+      fetchBalance();
+    }
+  };
+
+  useEffect(() => {
+    connectWallet()
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+      <div className="container py-3">
+        <Header balance={balance} />
+        <Tabs
+          addCar={addCar}
+          cars={cars}
+          buyCar={buyCar}
+          likeCar={likeCar}
+          dislikeCar={dislikeCar}
+          address={address}
+        />
+      </div>
+    </>
   );
 }
 
